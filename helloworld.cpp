d44e5ca87dc73afb82d6c487390e0110a8a96541
MATTMEDDY <matt.meddy@gmail.com>
2017-06-09 22:05:22 -0700

    Created Hello, World program in C++ language

---
+++ helloworld.cpp
@@ -0,0 +1,16 @@
+/*
+ *Author: Matt Medlock
+ *Date: 2017-06-09
+ *Description: Hello world program in C++ language
+ */
+
+#include <iostream>
+
+int main() {
+	
+	//output "Hello, world!"
+	std::cout << "Hello, world!" << std::endl;
+	
+	return 0;
+
+}
